fa87c758032c5b1bc462aa3c6911ad75694c2304
Jaime Forcada <jaime.forcada.balaguer@gmail.com>
2018-08-25 19:17:19 +0200

    first version of the blog

---
+++ Readme.md
@@ -0,0 +1,19 @@
+PWeb
+====
+
+To compile the site:
+
+    $ python pweb.py
+
+To serve it:
+
+    $ cd dist/ && python -m http.server
+
+TODO
+----
+- Improve latest.html without messing with repo
+- Add tags for blog posts
+- Write first post
+- Automate deployment with ansible
+- Get VM in Digital Ocean, terraform
+- Post tags
--- pweb.py
+++ pweb.py
@@ -1,10 +1,12 @@
 import os
+import glob
 import shutil
-from jinja2 import Environment, PackageLoader, select_autoescape
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
 CONTENT_DIRECTORY = 'content'
 TEMPLATE_SUBDIRECTORY = 'templates'
+POSTS_SUBSUBDIRECTORY = 'posts'
 STATICS_DIRECTORY = 'static'
 TEMPLATE_SUBDIRECTORY_FULL = '{0}/{1}'.format(
     CONTENT_DIRECTORY, TEMPLATE_SUBDIRECTORY)
@@ -12,16 +14,22 @@ DISTRIBUTION_DIRECTORY = 'dist'
 
 
 def url(value):
+    if 'post:' in value:
+        _, postnum = value.split(':')
+        post_name = glob.glob('content/templates/posts/{}-*.html'.format(postnum))[0]
+        return 'posts/{}'.format(os.path.basename(post_name))
+
     LINKS = {
-        'cv': 'cv.html',
-        'contact': 'contact.html',
-        'blog': 'blog.html',
+        'cv': '/cv.html',
+        'contact': '/contact.html',
+        'blog': '/blog.html',
+        'old-posts': '/old-posts.html'
     }
     return LINKS[value]
 
 
 env = Environment(
-    loader=PackageLoader(CONTENT_DIRECTORY, TEMPLATE_SUBDIRECTORY),
+    loader=FileSystemLoader(TEMPLATE_SUBDIRECTORY_FULL, followlinks=True),
     autoescape=select_autoescape(['html'])
 )
 env.filters['url'] = url
@@ -31,14 +39,40 @@ if os.path.isdir(DISTRIBUTION_DIRECTORY):
     shutil.rmtree(DISTRIBUTION_DIRECTORY)
 os.mkdir(DISTRIBUTION_DIRECTORY)
 
-# Generate templates
-# TODO: Recursive to recreate structure in distribution directory
+
+# Generate blog
+post_distribution_path = 'dist/posts'
+os.mkdir(post_distribution_path)
+last_post = None
+for post_filename in sorted(os.listdir(path='content/templates/posts')):
+    if post_filename[-5:] == '.html':
+        last_post = post_filename
+        template = env.get_template('posts/{}'.format(post_filename))
+        distribution_path = '{}/{}'.format(post_distribution_path,
+                                           post_filename)
+        template.stream(the='variables', go='here').dump(distribution_path)
+
+# Link last post to include it in the main blog page
+shutil.copyfile('content/templates/posts/{}'.format(last_post),
+                'content/templates/posts/latest.html')
+last_post_content = ''
+with open('content/templates/posts/latest.html') as latest:
+    last_post_content = latest.read()
+last_post_content = last_post_content.replace('{% extends "blog.html" %}', '')
+with open('content/templates/posts/latest.html', 'w') as latest:
+    latest.write(last_post_content)
+
+# Generate main templates
+# TODO: Recursive to recreate structure in distribution directory to refactor
+# with blog
 for template_name in os.listdir(path=TEMPLATE_SUBDIRECTORY_FULL):
     if template_name[-5:] == '.html':
         template = env.get_template(template_name)
         distribution_path = '{}/{}'.format(DISTRIBUTION_DIRECTORY,
                                            template_name)
         template.stream(the='variables', go='here').dump(distribution_path)
+
+os.remove('content/templates/posts/latest.html')
 
 # Copy other statics into dist dir
 shutil.copytree(STATICS_DIRECTORY, '{}/{}'.format(
